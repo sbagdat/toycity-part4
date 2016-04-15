6711fdb734e94c7323492666678a7f95a7cc556f
Sıtkı Bağdat <sbagdat@gmail.com>
2016-04-15 20:07:30 +0300

    Add Analyzable module and methods

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,23 @@
 module Analyzable
-  # Your code goes here!
+  def average_price(products)
+    (products.inject(0){|sum, product| sum + product.price.to_f}/products.size).round(2)
+  end
+
+  def print_report(products)
+    report_text = "Average Price: $#{average_price(products)}\n"
+    report_text << "Inventory by Brand:\n"
+    count_by_brand(products).each{|key, value| report_text << "  - #{key}: #{value}\n"}
+    report_text << "Inventory by Name:\n"
+    count_by_name(products).each{|key, value| report_text << "  - #{key}: #{value}\n"}
+    report_text
+  end
+
+  [:name, :brand].each do |meth|
+    define_method("count_by_#{meth}") do |products|
+      result = {}
+      items = products.map {|product|product.send(meth.to_sym)}
+      items.uniq.each{|item| result[item] = items.count(item)}
+      result
+    end
+  end
 end
